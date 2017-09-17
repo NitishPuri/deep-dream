f63146ed3483dec26ebb30db0b504eb0dca1756b
Nitish Puri <npuri1903@gmail.com>
2017-09-17 17:41:43 +0530

    Add script to generate dreams.

---
+++ deepdream.py
@@ -0,0 +1,315 @@
+# Imports
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+import PIL.Image
+from IPython.display import clear_output, Image, display, HTML
+
+import tensorflow as tf
+
+# Load the model
+model_fn = 'tensorflow_inception_graph.pb'
+
+# Picking some internal layer. Note that we use outputs before applying the ReLU nonlinearity
+# to have non-zero gradients for features with negative initial activations.
+layer = 'mixed4d_3x3_bottleneck_pre_relu'
+channel = 100 # picking some feature channel to visualize
+
+# start with a gray image with a little noise
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+
+# creating TensorFlow session and loading the model
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with tf.gfile.FastGFile(model_fn, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+imagenet_mean = 117.0
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input':t_preprocessed})
+
+# See graph details
+layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
+feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+
+print('Number of layers', len(layers))
+print('Total number of feature channels:', sum(feature_nums))
+
+
+# Utility functions
+def showarray(a, fmt='jpeg', filename=None):
+    a = np.uint8(np.clip(a, 0, 1)*255)
+    f = BytesIO()
+    i = PIL.Image.fromarray(a)
+    if filename :
+        i.save(filename, fmt)
+    else :
+        i.save(f, fmt)
+        display(Image(data=f.getvalue()))
+    
+def visstd(a, s=0.1):
+    '''Normalize the image range for visualization'''
+    return (a-a.mean())/max(a.std(), 1e-4)*s + 0.5
+
+def T(layer):
+    '''Helper for getting layer output tensor'''
+    return graph.get_tensor_by_name("import/%s:0"%layer)
+
+# Naive visualization
+def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+    
+    img = img0.copy()
+    for i in range(iter_n):
+        g, score = sess.run([t_grad, t_score], {t_input:img})
+        # normalizing the gradient, so the same step size should work 
+        g /= g.std()+1e-8         # for different layers and networks
+        img += g*step
+        print(score, end = ' ')
+    clear_output()
+    showarray(visstd(img))
+
+# Multiscale Image Generation
+def tffunc(*argtypes):
+    '''Helper that transforms TF-graph generating function into a regular one.
+    See "resize" function below.
+    '''
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+        return wrapper
+    return wrap
+
+# Helper function that uses TF to resize an image
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+resize = tffunc(np.float32, np.int32)(resize)
+
+
+def calc_grad_tiled(img, t_grad, tile_size=512):
+    '''Compute the value of tensor t_grad over the image in a tiled way.
+    Random shifts are applied to the image to blur tile boundaries over 
+    multiple iterations.'''
+    sz = tile_size
+    h, w = img.shape[:2]
+    sx, sy = np.random.randint(sz, size=2)
+    img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+    grad = np.zeros_like(img)
+    for y in range(0, max(h-sz//2, sz),sz):
+        for x in range(0, max(w-sz//2, sz),sz):
+            sub = img_shift[y:y+sz,x:x+sz]
+            g = sess.run(t_grad, {t_input:sub})
+            grad[y:y+sz,x:x+sz] = g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0)
+
+
+# Laplacian Pyramid Gradient Normalization
+k = np.float32([1,4,6,4,1])
+k = np.outer(k, k)
+k5x5 = k[:,:,None,None]/k.sum()*np.eye(3, dtype=np.float32)
+
+def lap_split(img):
+    '''Split the image into lo and hi frequency components'''
+    with tf.name_scope('split'):
+        lo = tf.nn.conv2d(img, k5x5, [1,2,2,1], 'SAME')
+        lo2 = tf.nn.conv2d_transpose(lo, k5x5*4, tf.shape(img), [1,2,2,1])
+        hi = img-lo2
+    return lo, hi
+
+def lap_split_n(img, n):
+    '''Build Laplacian pyramid with n splits'''
+    levels = []
+    for i in range(n):
+        img, hi = lap_split(img)
+        levels.append(hi)
+    levels.append(img)
+    return levels[::-1]
+
+def lap_merge(levels):
+    '''Merge Laplacian pyramid'''
+    img = levels[0]
+    for hi in levels[1:]:
+        with tf.name_scope('merge'):
+            img = tf.nn.conv2d_transpose(img, k5x5*4, tf.shape(hi), [1,2,2,1]) + hi
+    return img
+
+def normalize_std(img, eps=1e-10):
+    '''Normalize image by making its standard deviation = 1.0'''
+    with tf.name_scope('normalize'):
+        std = tf.sqrt(tf.reduce_mean(tf.square(img)))
+        return img/tf.maximum(std, eps)
+
+def lap_normalize(img, scale_n=4):
+    '''Perform the Laplacian pyramid normalization.'''
+    img = tf.expand_dims(img,0)
+    tlevels = lap_split_n(img, scale_n)
+    tlevels = list(map(normalize_std, tlevels))
+    out = lap_merge(tlevels)
+    return out[0,:,:,:]
+
+# Showing the lap_normalize graph with TensorBoard
+# lap_graph = tf.Graph()
+# with lap_graph.as_default():
+#     lap_in = tf.placeholder(np.float32, name='lap_in')
+#     lap_out = lap_normalize(lap_in)
+# show_graph(lap_graph)
+
+def render_lapnorm(t_obj, img0=img_noise, visfunc=visstd,
+                   iter_n=10, step=1.0, octave_n=3, octave_scale=1.4, lap_n=4, fileToSave=None):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+    # build the laplacian normalization graph
+    lap_norm_func = tffunc(np.float32)(partial(lap_normalize, scale_n=lap_n))
+
+    img = img0.copy()
+    for octave in range(octave_n):
+        if octave>0:
+            hw = np.float32(img.shape[:2])*octave_scale
+            img = resize(img, np.int32(hw))
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            g = lap_norm_func(g)
+            img += g*step
+            print('.', end = ' ')
+        clear_output()
+    showarray(visfunc(img), filename=fileToSave)
+
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.5, octave_n=4, octave_scale=1.4, filename='test.jpg'):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+    # split the image into a number of octaves
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+    
+    # generate details octave by octave
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+        clear_output()
+        filename = filename.split('.')
+        filename = filename[0] + '_o_{}.jpg'.format(octave)
+        showarray(img/255.0, filename=filename)
+
+
+def generate_layernames():    
+    import pandas as pd
+    d = {'layers' : layers, "features" : feature_nums}
+    ldf = pd.DataFrame(data=d)
+    ldf.to_csv('layer.csv')
+    print(ldf.head())
+
+def generate_rand_linear_lapnorm():
+    count = np.random.choice([1,2,3])
+
+    # tensor to be optimized
+    t = None
+
+    layer_idx = np.random.randint(0, len(layers))
+
+    features_idx = []
+    features_str = ''
+
+    for i in range(count):
+        features_idx.append(np.random.randint(0, feature_nums[layer_idx]))
+
+    features_idx.sort()
+
+    layer = layers[layer_idx]
+    layer = layer.split('/')[1]
+    
+    for i in range(count):
+        feature_idx = features_idx[i]
+
+        features_str += '{}'.format(feature_idx)
+        if(i<count-1):
+            features_str+=','
+
+        if(i == 0):
+            t = T(layer)[:,:,:,feature_idx]
+        else:
+            t += T(layer)[:,:,:,feature_idx]
+
+        print("Mixing ||{}|| Layer {} :: {} :: feature : {}".format(i, layer_idx, layer, feature_idx))
+
+    filename = 'dreamTemple/l_{}_f_{}.jpg'.format(layer_idx, features_str)
+
+    if os.path.isfile(filename):
+        "Skipping as it already exists"
+        return
+
+    render_lapnorm(t , fileToSave=filename, octave_n=4)
+    print("Saved to {}".format(filename))    
+    
+
+def generate_rand_dream(img=img_noise):
+    count = np.random.choice([1,2,3])
+
+    # tensor to be optimized
+    t = None
+
+    layer_idx = np.random.randint(0, len(layers))
+
+    features_idx = []
+    features_str = ''
+
+    for i in range(count):
+        features_idx.append(np.random.randint(0, feature_nums[layer_idx]))
+
+    features_idx.sort()
+
+    layer = layers[layer_idx]
+    layer = layer.split('/')[1]
+    
+    for i in range(count):
+        feature_idx = features_idx[i]
+
+        features_str += '{}'.format(feature_idx)
+        if(i<count-1):
+            features_str+=','
+
+        if(i == 0):
+            t = T(layer)[:,:,:,feature_idx]
+        else:
+            t += T(layer)[:,:,:,feature_idx]
+
+        print("Mixing ||{}|| Layer {} :: {} :: feature : {}".format(i, layer_idx, layer, feature_idx))
+
+    filename = 'dreamTemple/l_{}_f_{}.jpg'.format(layer_idx, features_str)
+
+    if os.path.isfile(filename):
+        "Skipping as it already exists"
+        return
+
+    render_lapnorm(t , fileToSave=filename, octave_n=4)
+    print("Saved to {}".format(filename))    
+    
+
+for i in range(50):
+    # generate_rand_lapnorm()
+    generate_rand_linear_lapnorm()
+
+
+# render_lapnorm(T(layer)[:,:,:,channel], fileToSave='testOutput.jpg')
+
+
