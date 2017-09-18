00d4202639f7a3346fda1f5b4ec3b6ec44dc5c5e
Nitish Puri <npuri1903@gmail.com>
2017-09-18 23:05:46 +0530

    utility script for image conversion.

--- rename.py
+++ rename.py
@@ -1,16 +1,21 @@
 import numpy as np
 import os
 import glob
+from PIL import Image
 
-
-INPUT_IMAGES = '../test_data/nd/*.jpg'
+INPUT_IMAGES = '../test_data2/g/*.png'
 
 index = 0
 for file in glob.iglob(INPUT_IMAGES):
-    newName = '../test_data/nd/{}.jpg'.format(index)
+    newName = '../test_data2/g/{}.jpg'.format(index)
     while os.path.isfile(newName) :
         index += 1
-        newName = '../test_data/nd/{}.jpg'.format(index)
+        newName = '../test_data2/g/{}.jpg'.format(index)
 
     print("{} --> {}".format(file, newName))        
-    os.rename(file, newName)
+
+    im = Image.open(file)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(newName)
+
+    # os.rename(file, newName)
